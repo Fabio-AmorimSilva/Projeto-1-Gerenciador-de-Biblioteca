fb956720748744e8fd7b4b6d7e9a1186aa2ed8d0
Fabio <fabioamorin120@gmail.com>
2024-11-29 19:38:43 -0300

    feat: Adding exception middleware

---
+++ LibraryManagement/LibraryManagement.Api/ExceptionHandler/ExceptionMiddlewareHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace LibraryManagement.Api.ExceptionHandler;
+
+public class ExceptionMiddlewareHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        var error = new
+        {
+            exception.Message,
+            exception.StackTrace,
+            exception.InnerException
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(error, cancellationToken: cancellationToken);
+
+        return true;
+    }
+}
