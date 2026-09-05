b05153588dece387d601259805f52082b215d47c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-09-05 16:30:38 -0300

    feat: Creating frontend project and some features

---
+++ src/frontend/LibraryManagement/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/layout/main-layout/main-layout')
+        .then((m => m.MainLayout))
+  }
+];
---
+++ src/frontend/LibraryManagement/src/app/features/books/book-genre-card/book-genre-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-genre-card',
+  imports: [],
+  templateUrl: './book-genre-card.html',
+  styleUrl: './book-genre-card.scss',
+})
+export class BookGenreCard {}
