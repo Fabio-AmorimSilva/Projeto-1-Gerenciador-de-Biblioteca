c253829917f80b176bd6e231d94c1bff9db93904
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-09-06 15:29:03 -0300

    feat: Adding book-card component

--- src/frontend/LibraryManagement/src/app/core/layout/main-layout/main-layout.ts
+++ src/frontend/LibraryManagement/src/app/core/layout/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../nav-bar/nav-bar';
+import { BookCard } from '../../../features/books/components/book-card/book-card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavBar, RouterOutlet],
+  imports: [NavBar, BookCard, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
