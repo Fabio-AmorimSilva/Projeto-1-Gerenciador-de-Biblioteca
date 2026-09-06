b0e7fbc5ee293327d86ce4c0363c9e8ab184bc82
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-09-06 15:23:23 -0300

    Moving to another folder

--- src/frontend/LibraryManagement/src/app/features/books/book-genre-card/book-genre-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-book-genre-card',
-  imports: [],
-  templateUrl: './book-genre-card.html',
-  styleUrl: './book-genre-card.scss',
-})
-export class BookGenreCard {}
