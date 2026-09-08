49149baa22048f31f06af283f8ebd27cc20ad375
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-09-08 12:38:42 -0300

    feat: Adding new routes

--- src/frontend/LibraryManagement/src/app/app.routes.ts
+++ src/frontend/LibraryManagement/src/app/app.routes.ts
@@ -5,6 +5,21 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./core/layout/main-layout/main-layout')
-        .then((m => m.MainLayout))
+        .then((m => m.MainLayout)),
+        
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/books/components/book-card/book-card')
+            .then(m => m.BookCard)
+      },
+      {
+        path: 'book-details/:id',
+        loadComponent: () =>
+          import('./features/books/components/book-details/book-details')
+            .then(m => m.BookDetails)
+      }
+    ]
   }
 ];
--- src/frontend/LibraryManagement/src/app/core/layout/main-layout/main-layout.ts
+++ src/frontend/LibraryManagement/src/app/core/layout/main-layout/main-layout.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../nav-bar/nav-bar';
-import { BookCard } from '../../../features/books/components/book-card/book-card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavBar, BookCard, RouterOutlet],
+  imports: [NavBar, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
