afe29ccd565f62aa69b2d5cadaf2fe22f1c9a482
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-09-10 19:10:33 -0300

    feat: Adding LoginUserDto

---
+++ src/backend/LibraryManagement.Application/Dtos/Users/LoginUserDto.cs
@@ -0,0 +1,24 @@
+﻿namespace LibraryManagement.Application.Dtos.Users;
+
+public record LoginUserDto(
+    string Email, 
+    string Password,
+    IEnumerable<string> Roles,
+    IEnumerable<string> Permissions
+);
+
+public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+{
+    public LoginUserDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(LoginUserDto.Email)))
+            .EmailAddress()
+            .WithMessage(ErrorMessages.EmailIsNotValid(nameof(LoginUserDto.Email)));
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(LoginUserDto.Password)));
+    }
+}
