a3ffbce29dc1381beed900b355cb458b4a5379f3
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-09-10 19:09:41 -0300

    feat: Creating PasswordHashService

---
+++ src/backend/LibraryManagement.Application/Common/IPasswordService.cs
@@ -0,0 +1,6 @@
+﻿namespace LibraryManagement.Application.Common;
+
+public interface IPasswordService
+{
+    string HashPassword(string password);
+}
---
+++ src/backend/LibraryManagement.Infrastructure/Auth/Services/PasswordHashService.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryManagement.Infrastructure.Auth.Services;
+
+public class PasswordHashService : IPasswordService
+{
+    public string HashPassword(string password)
+    {
+        string combinedHash = password;
+
+        using (var sha256 = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(combinedHash);
+            
+            byte[] hash = sha256.ComputeHash(bytes);
+            
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+                builder.Append(hash[i].ToString("x2"));
+            
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
